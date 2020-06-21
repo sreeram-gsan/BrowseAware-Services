5686215ab12179df5dc42ae871b028992511e440
sreeram-gsan <sreeram2910@gmail.com>
2020-06-21 18:21:02 +0000

    cleanup

--- ChromeExtensionService/common/LoginForm.py
+++
@@ -1,5 +0,0 @@
-from wtforms.validators import DataRequired
-
-class LoginForm(Form):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
